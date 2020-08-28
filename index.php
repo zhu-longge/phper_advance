87e055659be2e253ddd7e021a068c486819b7a7d
zhuyilong@sovell.com <zhu8047497>
2020-08-28 15:37:25 +0800

    singleton

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
 <?php 
-	include_once 'singleton.php';
-
-	//调用数据库连接单例类
-	$sing_db = Singleton::getInstance('127.0.0.1', 'root', '123456', 'phper_advance');
-	$res = $sing_db->query('select * from user');
-	print_r($res->fetch_assoc());
+	echo 'php进阶学习系列~';
--- singleton.php
+++ singleton.php
@@ -8,6 +8,10 @@
 
 		private static $instance; //定义实例属性变量
 
+
+		/**
+	 	 * 私有化构造方法,防止直接实例化
+	 	 */
 		private function __construct($host, $username, $password, $database) {
 			$this->db_conn = new mysqli($host, $username, $password, $database);
 			if (!$this->db_conn) {
@@ -56,3 +60,8 @@
 			return $this->db_conn->close();
 		}
 	}
+
+	//调用数据库连接单例类
+	$sing_db = Singleton::getInstance('127.0.0.1', 'root', '123456', 'phper_advance');
+	$res = $sing_db->query('select * from user');
+	print_r($res->fetch_assoc());
